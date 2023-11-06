a1530a4de3b194a80dc4be3941477f7c55ff79fc
chenyutao123 <148777241+chenyutao123@users.noreply.github.com>
2023-11-06 02:53:22 +0000

    added data

--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -20,6 +20,137 @@ import pandas as pd
 import streamlit as st
 from streamlit.hello.utils import show_code
 
+import numpy as np
+import pandas as pd
+
+# Read the data file
+data = pd.read_csv('hotel_bookings(1).csv')
+
+# Data preprocessing
+# Drop missing values
+data = data.dropna()
+
+# Question 1: Calculate the average length of stay for each hotel
+average_stay = data.groupby('hotel')['stays_in_week_nights'].mean()
+print("Question 1: Average length of stay for each hotel")
+print(average_stay)
+print()
+
+# Question 2: Calculate the cancellation rate for each month
+# Convert arrival_date to datetime format
+data['arrival_date'] = pd.to_datetime(data['arrival_date_year'].astype(str) + '-' + data['arrival_date_month'] + '-' + data['arrival_date_day_of_month'].astype(str))
+# Extract year and month from arrival_date
+data['year_month'] = data['arrival_date'].dt.to_period('M')
+# Calculate cancellation rate by grouping data by year_month and taking the mean of is_canceled
+cancellation_rate = data.groupby('year_month')['is_canceled'].mean()
+print("Question 2: Cancellation rate for each month")
+print(cancellation_rate)
+print()
+
+# %%
+import numpy as np
+import pandas as pd
+
+# Read the data file
+data = pd.read_csv('hotel_bookings(1).csv')
+
+# Data preprocessing
+# Drop missing values
+data = data.dropna()
+
+# Question 1: Calculate the number of bookings for each market segment and distribution channel
+bookings_by_segment_channel = data.groupby(['market_segment', 'distribution_channel'])['hotel'].count()
+print("Question 1: Number of bookings for each market segment and distribution channel")
+print(bookings_by_segment_channel)
+print()
+
+# Question 2: Calculate the average cancellation rate for each market segment and distribution channel
+cancellation_rate_by_segment_channel = data.groupby(['market_segment', 'distribution_channel'])['is_canceled'].mean()
+print("Question 2: Average cancellation rate for each market segment and distribution channel")
+print(cancellation_rate_by_segment_channel)
+print()
+
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the data file
+hotel_booking_frame = pd.read_csv('hotel_bookings(1).csv')
+
+# Group by hotel and assigned room type, calculate the mean of adr
+assigned_price_frame = hotel_booking_frame.groupby(["hotel", "assigned_room_type"])["adr"].mean().reset_index()
+
+# Pivot process
+pivot_df = assigned_price_frame.pivot_table(index='hotel', columns='assigned_room_type', values='adr')
+
+# Display the pivot table
+pivot_df  # This will display the pivot table with average adr for each hotel and assigned room type
+
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Set the plot style
+plt.style.use("ggplot")
+
+# Obtain the dataset
+hotel_booking_frame = pd.read_csv('hotel_bookings(1).csv')
+
+# Convert the date string to datetime
+hotel_booking_frame['reservation_status_date'] = pd.to_datetime(hotel_booking_frame['reservation_status_date'])
+
+# Separate the data of different hotels
+resort_hotel = hotel_booking_frame[hotel_booking_frame['hotel'] == 'Resort Hotel']
+city_hotel = hotel_booking_frame[hotel_booking_frame['hotel'] == 'City Hotel']
+
+# Group by reservation status date and calculate the mean of adr
+resort_hotel = resort_hotel.groupby('reservation_status_date')[['adr']].mean()
+city_hotel = city_hotel.groupby('reservation_status_date')[['adr']].mean()
+
+# Print the mean adr of resort hotel over time
+print("Resort Hotel:")
+print(resort_hotel)
+
+# Print the mean adr of city hotel over time
+print("City Hotel:")
+print(city_hotel)
+
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.style.use("ggplot")
+
+# Obtain the dataset
+hotel_booking_frame = pd.read_csv('hotel_bookings(1).csv')
+
+# Convert the date string to datetime
+hotel_booking_frame['reservation_status_date'] = pd.to_datetime(hotel_booking_frame['reservation_status_date'])
+
+# Separate the data of different hotels
+resort_hotel = hotel_booking_frame[hotel_booking_frame['hotel'] == 'Resort Hotel']
+city_hotel = hotel_booking_frame[hotel_booking_frame['hotel'] == 'City Hotel']
+
+# Assign month column
+resort_hotel = resort_hotel.assign(month=resort_hotel["reservation_status_date"].dt.strftime("%m"))
+city_hotel = city_hotel.assign(month=city_hotel["reservation_status_date"].dt.strftime("%m"))
+
+# Group by month and calculate the sum of adr
+total_adr_resort = resort_hotel.groupby(["month"])["adr"].sum()
+total_adr_city = city_hotel.groupby(["month"])["adr"].sum()
+
+# Print the total adr of resort hotel for each month
+print("Resort Hotel:")
+print(total_adr_resort)
+
+# Print the total adr of city hotel for each month
+print("City Hotel:")
+print(total_adr_city)
+
+# %%
+
+
+
 
 def data_frame_demo():
     @st.cache_data
